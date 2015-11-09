36e923c6f146746fbad7277323eb225322d23165
綿貫岳海 <takemi007@hotmail.com>
2015-11-10 02:11:28 +0900

    +++

--- TechCameraSwift/ViewController.swift
+++ TechCameraSwift/ViewController.swift
@@ -26,12 +26,26 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         let picker = UIImagePickerController()
         picker.sourceType = UIImagePickerControllerSourceType.Camera
         picker.delegate = self
+        
+        //カメラを正方形の形に開く
+        picker.allowsEditing = true
+        
         presentViewController(picker, animated: true, completion: nil)
     }
     
-    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
-        cameraImageView.image = image                             // 撮影した画像をセットする
-        filteredImage = image
+//    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
+//        cameraImageView.image = image
+//        
+
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        
+        cameraImageView.image = info[UIImagePickerControllerEditedImage] as? UIImage
+    
+        filteredImage = cameraImageView.image
+        
+        // 撮影した画像をセットする
+        //filteredImage = image
+        
         
         // ----- 合成した画像を保存する
         UIGraphicsBeginImageContext(cameraImageView.bounds.size)
@@ -44,8 +58,16 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     
     @IBAction func savePhoto(){
-        
+        UIImageWriteToSavedPhotosAlbum(cameraImageView.image!, self, "image:didFinishSavingWithError:contextInfo:", nil)
     }
+    
+    //保存処理のイベント
+    func image(image: UIImage, didFinishSavingWithError error: NSError!, contextInfo: UnsafeMutablePointer<Void>) {
+        if error != nil {
+            //エラーの時
+        }
+    }
+    
     
     @IBAction func sepiaFilter(){
         // image が 元画像のUIImage
@@ -57,7 +79,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         let cgimg:CGImageRef = ciContext.createCGImage(ciFilter.outputImage!, fromRect:ciFilter.outputImage!.extent)
         
         //image2に加工後のUIImage
-        let image2:UIImage = UIImage(CGImage: cgimg, scale: 1.0, orientation:UIImageOrientation.Up)
+        let image2:UIImage = UIImage(CGImage: cgimg)//, scale: 1.0, orientation:UIImageOrientation.Right)//UIImageOrientation.up
         
         cameraImageView.image = image2
     }
